76ad8be64c94dd7df537eb5ed5e9d417bf1aeb76
Shreyas Kapur <shreyaskapur@yahoo.com>
2020-07-29 12:42:54 -0400

    Unify common fetch method across API calls.

--- sharp/js/class.js
+++ sharp/js/class.js
@@ -1,3 +1,27 @@
+export class Core {
+    static makeRequest(route, body) {
+        return new Promise((resolve, reject) => {
+            fetch(route, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            }).then((r) => {
+                r.json().then((data) => {
+                    if ("error" in data) {
+                        reject(data.error);
+                        return;
+                    }
+                    resolve(data.result);
+                });
+            }).catch((e) => {
+                reject(e);
+            })
+        });
+    }
+}
+
 export class API {
     {{ functions }}
 }
--- sharp/js/function.js
+++ sharp/js/function.js
@@ -3,27 +3,10 @@ static {{ name }} (
         {{ parameter }},
     {%- endfor -%}
 ) {
-    return new Promise((resolve, reject) => {
-        fetch("{{route}}", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                {% for parameter in parameters -%}
-                {{ parameter }}: {{ parameter }},
-                {% endfor -%}
-            }),
-        }).then((r) => {
-            r.json().then((data) => {
-                if ("error" in data) {
-                    reject(data.error);
-                    return;
-                }
-                resolve(data.result);
-            });
-        }).catch((e) => {
-            reject(e);
-        })
+
+    return Core.makeRequest("{{route}}", {
+        {% for parameter in parameters -%}
+        {{ parameter }}: {{ parameter }},
+        {% endfor -%}
     });
 }
